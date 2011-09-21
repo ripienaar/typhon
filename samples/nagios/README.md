318f4e6e36bfb3a99cf03a3af66108df98ee2c18
R.I.Pienaar <rip@devco.net>
2011-09-21 14:18:48 +0100

    Add a working sample

---
+++ samples/nagios/README.md
@@ -0,0 +1,31 @@
+A simple example using Nagios to write results of every check
+it does to a file then using Typhon to parse this file and emit
+events over Stomp to a event archive/handler.
+
+First create a performance data handler for nagios:
+
+    #! /bin/sh
+    TIMET=$1
+    HOSTNAME=$2
+    SERVICEDESC=$3
+    OUTPUT=$4
+    SERVICESTATE=$5
+    PERFDATA=$6
+
+    /usr/bin/printf "%b" "$TIMET\t$HOSTNAME\t$SERVICEDESC\t$OUTPUT\t$SERVICESTATE\t$PERFDATA\n" >> /var/tmp/typhon_input
+
+Now create the nagios command to call this script:
+
+    define command{
+        command_name                    process-service-perfdata
+	command_line                    $USER1$/process-service-perfdata  "$LASTSERVICECHECK$" "$HOSTNAME$" "$SERVICEDESC$" "$SERVICEOUTPUT$" "$SERVICESTATE$" "$SERVICEPERFDATA$"
+    }
+
+And tell nagios to call your command after every check:
+
+    service_perfdata_command=process-service-perfdata
+
+Now install the _nagios\_perf\_head.rb_ and setup typhon with your
+Stomp server and you should start seeing events on the topic.
+
+Remember to rotate the log file that the above script creates
---
+++ samples/nagios/nagios_perf_head.rb
@@ -0,0 +1,34 @@
+require 'json'
+
+class Typhon
+    grow(:name => "nagios_perf", :files => "/var/tmp/typhon_input") do |file, pos, line|
+        if line =~ /^\d+/
+            exitcodes = {"OK" => 0, "WARNING" => 1, "CRITICAL" => 2, "UNKNOWN" => 3}
+            alert_data = {}
+
+            timestamp, host, check, output, exitcode, perfdata = line.split("\t")
+
+            parsed_perf = {}
+
+            if perfdata
+                perfdata.split(/ /).each do |perf|
+                    perf.scan(/'*([^=']+?)'*=([^a-zA-Z]+?)[a-zA-Z\%]*?;/).each do |metric|
+                        parsed_perf[ metric[0].strip ] = metric[1].strip
+                    end
+                end
+            end
+
+            alert_data["eventtime"] = timestamp.to_i
+            alert_data["subject"] = host
+            alert_data["name"] = check
+            alert_data["text"] = output
+            alert_data["severity"] = exitcodes[exitcode].to_i
+            alert_data["metrics"] = parsed_perf
+            alert_data["origin"] = "monitor1.pinetecltd.net"
+            alert_data["type"] = "status"
+            alert_data["tags"] = {}
+
+            stomp.publish("/topic/nagios.checks", alert_data.to_json)
+        end
+    end
+end
